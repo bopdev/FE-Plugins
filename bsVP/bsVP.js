c5440620def7b67d6c44925c92e51e4c73feee4d
JimmySevere <JimmySevere@users.noreply.github.com>
2015-10-21 16:07:15 +0100

    A lot of additional functionality. Primarily callbacks for conditions met/not met.

--- bsVP/bsVP.js
+++ bsVP/bsVP.js
@@ -10,14 +10,46 @@
 	/**
 	 * jQuery function bsVP
 	 * 
+	 * 
+	 * bsVP(size [, comp])
+	 * 
 	 * @param int/string size - The cutoff viewport width
 	 * 	(BS CSS column size or #pxs).
+	 * 	(required)
 	 * @param string comp - string representing the comparison operator.
-	 * 
+	 * 	E.g. '>' empties if window width is smaller than size.
+	 * 	(optional) default: '>'.
 	 * @return object $obj - This jQuery object either unchanged or 
 	 * 	emptied.
+	 * 
+	 * 
+	 * bsVP(size, incb [, outcb])
+	 * 
+	 * @param int/string size - The cutoff viewport width
+	 * 	(BS CSS column size or #pxs).
+	 * 	(required)
+	 * @param function incb - function on viewport width > size.
+	 * 	(required)
+	 * @param function outcb - function on viewport width <= size.
+	 * 	(optional) default: do nothing.
+	 * @return object $obj - this
+	 * 
+	 * 
+	 * bsVP(size, comp, incb [, outcb])
+	 * 
+	 * @param int/string size - The cutoff viewport width
+	 * 	(BS CSS column size or #pxs).
+	 * 	(required)
+	 * @param string comp - string representing the comparison operator.
+	 * 	E.g. '>' empties if window width is smaller than size.
+	 * 	(required)
+	 * @param function incb - function on conditions met.
+	 * 	(required)
+	 * @param function outcb - function on conditions not met.
+	 * 	(optional) default: do nothing.
+	 * @return object $obj - this
 	 */
-	$.fn.bsVP = function(size, comp){
+	$.fn.bsVP = function(size, comp, incb, outcb){
 		if(typeof size == 'undefined') return this;
 		switch(size){
 			case 'xs':
@@ -39,34 +71,46 @@
 		
 		size = parseInt(size);
 		var ww = $(window).width();
+		var isIn = false;
 		
-		switch(comp){
-			case '<=':
-				if(ww <= size)
-					return this;
-			break;
-			case '==':
-				if(ww == size)
-					return this;
-			break;
-			case '!=':
-				if(ww == size)
-					return this;
-			break;
-			case '>=':
-				if(ww >= size)
-					return this;
-			break;
-			case '<':
-				if(ww < size)
-					return this;
-			break;
-			case '>':
-			default:
-				if(ww > size)
-					return this;
+		if(typeof comp == 'function'){
+			outcb = incb;
+			incb = comp;
+			isIn = ww > size;
+		}else{
+			switch(comp){
+				case '<=':
+					isIn = ww <= size;
+				break;
+				case '==':
+					isIn = ww == size;
+				break;
+				case '!=':
+					isIn = ww != size;
+				break;
+				case '>=':
+					isIn = ww >= size;
+				break;
+				case '<':
+					isIn = ww < size;
+				break;
+				case '>':
+				default:
+					isIn = ww > size;
+			}
 		}
 		
-		return this.not('*');
+		
+		if(typeof incb == 'function'){
+			if(isIn){
+				this.each(incb);
+			}else if(typeof outcb == 'function')
+				this.each(outcb);
+			return this;
+		}else{
+			if(isIn)
+				return this;
+			return this.not('*');
+		}
 	};
 })(jQuery);
